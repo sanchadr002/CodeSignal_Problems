c7646eb6f1d0a784ea3f6f0c5c890b5f2afb7e48
sanchadr002 <95728490+sanchadr002@users.noreply.github.com>
2022-07-21 12:15:35 -0700

    Create Add.js

---
+++ Add.js
@@ -0,0 +1,8 @@
+// write a function that returns sum of two numbers
+
+// can accomplish this with a simple algebra formula
+
+function solution(param1, param2) {
+    const sum = (param1 + param2)
+    return sum
+}
